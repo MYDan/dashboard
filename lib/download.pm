3ddfad6494529faeba4f160080118d784c0d04d6
lijinfeng2011 <lijinfeng2011@gmail.com>
2017-07-11 22:25:00 +0800

    update

--- lib/download.pm
+++ lib/download.pm
@@ -9,7 +9,7 @@ our $VERSION = '0.1';
 any '/download/' => sub {
     my $param = params();
     my %download;
-    map{ push @{$download{$1}}, $2 if $_ =~ /\/([^\/]+)\/([^\/]+)$/ }glob "$RealBin/../public/download/*/*";
+    map{ push @{$download{$1}}, $2 if $_ =~ /\/([^\/]+)\/([^\/]+)$/ }glob "$RealBin/../public/download/agent/*/*";
 
     template 'download', +{ download => \%download,  serveraddr => config->{serveraddr} };
 };
